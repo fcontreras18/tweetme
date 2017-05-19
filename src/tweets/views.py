b6a8b4b83d3d5a5018e6db13f17c7301995dec5c
Francisco Echeverria <fcontreras18@gmail.com>
2017-05-19 16:16:12 -0700

    Add delete view

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,19 +1,23 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView, ListView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from django.views.generic import (
+                    CreateView,
+                    DetailView,
+                    DeleteView,
+                    ListView,
+                    UpdateView)
 
 from .forms import TweetModelForm
 from .mixins import FormUserNeededMixin, UserOwnerMixin
 from .models import Tweet
 
-# Create/Retrieve/Update/Delete
 
 # Create
 class TweetCreateView(FormUserNeededMixin, CreateView):
     form_class = TweetModelForm
     template_name = 'tweets/create_view.html'
     success_url = '/tweet/create/'
-    # login_url = '/admin/'
 
 # Update
 class TweetUpdateView(LoginRequiredMixin, UserOwnerMixin, UpdateView):
@@ -22,44 +26,23 @@ class TweetUpdateView(LoginRequiredMixin, UserOwnerMixin, UpdateView):
     template_name = 'tweets/update_view.html'
     success_url = '/tweet/'
 
+# Delete
+class TweetDeleteView(LoginRequiredMixin, DeleteView):
+    model = Tweet
+    template_name = 'tweets/delete_confirm.html'
+    success_url = reverse_lazy('home')
+
 
 # Retrieve
 class TweetDetailView(DetailView):
-    # template_name = "tweets/tweet_detail_view.html"
     queryset = Tweet.objects.all()
 
-    # def get_object(self):
-    #     print(self.kwargs)
-    #     pk = self.kwargs.get("pk")
-    #     print(pk)
-    #     return Tweet.objects.get(id=pk)
 
 class TweetListView(ListView):
-    # template_name ="tweets/list_view.html"
     queryset = Tweet.objects.all()
 
     def get_context_data(self, *args, **kwargs):
         context = super(TweetListView, self).get_context_data(*args, **kwargs)
-        # context["another_list"] = Tweet.objects.all()
-        # print(context)
+
         return context
 
-
-# def tweet_detail_view(request, id=1):
-#     obj = Tweet.objects.get(id=id) # GET from database
-#     print(obj)
-#     context = {
-#         "object": obj
-#     }
-#     return render(request, "tweets/detail_view.html", context)
-
-
-# def tweet_list_view(request):
-#     queryset = Tweet.objects.all()
-#     print(queryset)
-#     for obj in queryset:
-#         print(obj.content)
-#     context = {
-#         "object_list": queryset
-#     }
-#     return render(request, "tweets/list_view.html", context)
