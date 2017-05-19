301abf97e9b7ce4f69b31659f70486d8247d6b7d
Francisco Echeverria <fcontreras18@gmail.com>
2017-05-19 15:37:46 -0700

    Add mixin

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,21 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView, ListView, CreateView
+
 from .forms import TweetModelForm
+from .mixins import FormUserNeededMixin
 from .models import Tweet
-# Create your views here.
+
 # Create/Retrieve/Update/Delete
 
 # Create
-class TweetCreateView(CreateView):
+class TweetCreateView(LoginRequiredMixin, FormUserNeededMixin, CreateView):
     form_class = TweetModelForm
     template_name = 'tweets/create_view.html'
     success_url = '/tweet/create/'
+    # login_url = '/admin/'
 
-    def form_valid(self, form):
-        # This method is called when valid form data has been POSTed
-        # It should retrun an HttpResponse
-        form.instance.user = self.request.user
-        return super(TweetCreateView, self).form_valid(form)
+    # def form_valid(self, form):
+    #     if self.request.user.is_authenticated():
+    #         form.instance.user = self.request.user
+    #         return super(TweetCreateView, self).form_valid(form)
+    #     else:
+    #         form._errors[forms.forms.NON_FIELD_ERRORS] = ErrorList(["User must be logged in to continue"])
+    #         return self.form_invalid(form)
 
 
 # def tweet_create_view(request):
