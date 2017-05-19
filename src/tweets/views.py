48742d4f9f20286c0e7a95319bb046b2951dfb41
Francisco Echeverria <fcontreras18@gmail.com>
2017-05-19 14:21:42 -0700

    Create view

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, CreateView
+from .forms import TweetModelForm
 from .models import Tweet
 # Create your views here.
 # Create/Retrieve/Update/Delete
+
+# Create
+class TweetCreateView(CreateView):
+    form_class = TweetModelForm
+    template_name = 'tweets/create_view.html'
+    success_url = '/tweet/create/'
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed
+        # It should retrun an HttpResponse
+        form.instance.user = self.request.user
+        return super(TweetCreateView, self).form_valid(form)
+
+
+# def tweet_create_view(request):
+#     form = TweetModelForm(request.POST or None)
+#     if form.is_valid():
+#         instance = form.save(commit=false)
+#         instance.user = request.user
+#         instance.save()
+#     content = {
+#         "form":form
+#     }
+#     return render(request, 'tweets/create_view.html', context)
+
 
 # Retrieve
 class TweetDetailView(DetailView):
